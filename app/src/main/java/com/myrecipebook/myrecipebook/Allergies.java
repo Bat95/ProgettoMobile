97806f969b83732fcd9958341e441e6296890024
Sonia Biatel <sonia.biatel@gmail.com>
2017-05-12 17:05:07 +0200

    fragment added to the drawer

---
+++ app/src/main/java/com/myrecipebook/myrecipebook/Allergies.java
@@ -0,0 +1,28 @@
+package com.myrecipebook.myrecipebook;
+
+/**
+ * Created by Sonia on 12/05/17.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Allergies extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.allergies, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Allegies");
+    }
+}
