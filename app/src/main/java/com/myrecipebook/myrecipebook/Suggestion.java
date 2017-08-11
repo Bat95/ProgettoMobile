25ca183a14178fbf3e73c52aa9a89087489ec275
Sonia Biatel <sonia.biatel@gmail.com>
2017-08-11 19:03:47 +0200

    inizio di suggestion

--- app/src/main/java/com/myrecipebook/myrecipebook/Suggestion.java
+++ app/src/main/java/com/myrecipebook/myrecipebook/Suggestion.java
@@ -11,8 +11,16 @@ import android.os.Bundle;
         import android.view.View;
         import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Suggestion extends Fragment {
+
+    List<Recipe> NewRecipes;
+
+    //ad esempio quante ricette vuoi ricevere
+    int number = 6;
 
     @Nullable
     @Override
@@ -24,5 +32,22 @@ public class Suggestion extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Ricette suggerite");
+
+        // temporary! chiedo al server le news sulle nuove ricette aggiunte :) in pratica sono solo tot random
+        HttpHelper.Get(
+                        getActivity().getApplicationContext(),
+                        "http://646ffb06.ngrok.io/api/values/number",
+                        new BaseHttpResponseHandler<InfoDto>(InfoDto.class) {
+
+                            @Override
+                            public void handleResponse(InfoDto response) {
+                                NewRecipes = new ArrayList<>(response.resultRecipes);
+                            }
+
+                            @Override
+                            public void handleError(String errorMessage) {
+                                super.handleError(errorMessage);
+                            }
+                        });
     }
 }
