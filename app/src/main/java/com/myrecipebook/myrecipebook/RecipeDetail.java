8e54136e1abd76cc0eec38f6bac18302a0e0123c
Sonia Biatel <sonia.biatel@gmail.com>
2017-08-23 19:04:24 +0200

    dettaglio ricetta

--- app/src/main/java/com/myrecipebook/myrecipebook/RecipeDetail.java
+++ app/src/main/java/com/myrecipebook/myrecipebook/RecipeDetail.java
@@ -1,11 +1,22 @@
 package com.myrecipebook.myrecipebook;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+
 
 /**
  * Created by Sonia on 23/08/17.
@@ -19,8 +30,28 @@ public class RecipeDetail extends Fragment {
 
         Recipe detailRecipe = new Recipe();
 
+        TextView difficultyLabel = (TextView) view.findViewById(R.id.difficulty_value);
+        TextView timeLabel = (TextView) view.findViewById(R.id.time_value);
+        TextView doseLabel = (TextView) view.findViewById(R.id.dose_value);
+        TextView categoryLabel = (TextView) view.findViewById(R.id.category_value);
+        TextView stepsLabel = (TextView) view.findViewById(R.id.list_procedure);
+        ImageView imageRecipe = (ImageView) view.findViewById(R.id.detail_image);
 
+        ListView listIngred = (ListView) view.findViewById(R.id.list_ingredients);
 
+        difficultyLabel.setText(detailRecipe.Difficulty);
+        timeLabel.setText(detailRecipe.Duration);
+        doseLabel.setText(detailRecipe.DosePerPerson);
+        categoryLabel.setText(detailRecipe.Category);
+
+        listIngred.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, detailRecipe.Ingredients));
+
+        for (String item: detailRecipe.Steps) {
+            stepsLabel.append(item);
+            stepsLabel.append(" ");
+        }
+
+        new DownloadImageTask(imageRecipe).execute(detailRecipe.MainPic);
 
     }
 
@@ -28,5 +59,30 @@ public class RecipeDetail extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.recipe_details, container, false);
+    }
+}
+
+class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
     }
 }
